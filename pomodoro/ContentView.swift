42f87dd2a4eda0f8fb83c5a94bd2eb70270ce9e3
alanchiem <chiemalan@gmail.com>
2022-06-28 00:17:47 -0700

    adding changes to main

--- pomodoro/ContentView.swift
+++ pomodoro/ContentView.swift
@@ -6,7 +6,46 @@
 //
 
 import SwiftUI
-import CoreData
+import AVKit
+
+
+
+let defaultTimeRemaining: CGFloat = 1800
+let breakTime: CGFloat = 300
+let lineWidth: CGFloat = 7
+let radius: CGFloat = 150
+
+let breakColor: Color = Color.init(red: 249/255, green: 235/255, blue: 122/255)
+let workColor: Color = Color.init(red: 101/255, green: 252/255, blue: 244/255)
+let buttonColor: Color = Color.init(red: 1, green: 1, blue: 1)
+
+
+
+class SoundManager {
+    
+    static let instance = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    func playSound() {
+        
+        guard let url = Bundle.main.url(forResource: "bell", withExtension: ".mp3") else {return}
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback)
+            } catch(let error) {
+                print(error.localizedDescription)
+            }
+            player?.play()
+        } catch let error {
+            print("Error with sound .\(error.localizedDescription)")
+        }
+        
+            
+    }
+}
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -16,73 +55,70 @@ struct ContentView: View {
         animation: .default)
     private var items: FetchedResults<Item>
 
+    
+    @State private var isActive = false
+    @State private var timeRemaining: CGFloat = defaultTimeRemaining
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+        ZStack {
+                    Color.black.ignoresSafeArea() // 1
+        VStack(spacing: 25) {
+            ZStack {
+                Circle()
+                    .stroke(timeRemaining > breakTime ? Color.black : breakColor, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                
+                Circle()
+                    .trim(from: 0, to: ((defaultTimeRemaining - timeRemaining) / defaultTimeRemaining))
+                    // change color 22:50
+                    .stroke(timeRemaining > breakTime ? workColor : Color.black, style: StrokeStyle(lineWidth: timeRemaining > breakTime ? lineWidth : lineWidth + 1, lineCap: .butt))
+                    .rotationEffect(.degrees(-90))
+                
+
+
+               
+            }.frame(width: radius * 2, height: radius * 2, alignment: .center)
+                
+        
+            HStack(spacing: 25){
+                Label("Pause/Play", systemImage: "\(isActive ? "pause.fill" : "play.fill")")
+                    .foregroundColor(buttonColor)
+                    .onTapGesture(perform: {isActive.toggle()})
+                    .labelStyle(IconOnlyLabelStyle())
+                    .frame(alignment: .center)
+
+                Label("Reset", systemImage: "backward.fill")
+                    .foregroundColor(buttonColor)
+                    .onTapGesture(perform: {isActive = false
+                    timeRemaining = defaultTimeRemaining})
+                    .labelStyle(IconOnlyLabelStyle())
+                    .frame(alignment: .center)
+                
+            }.frame(alignment: .center)
         }
+        .onReceive(timer, perform: { _ in guard isActive else {return}
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                timeRemaining = defaultTimeRemaining
+                
+            }
+            if breakTime == timeRemaining || timeRemaining == 0 {
+                SoundManager.instance.playSound()
+            }
+            
+        })
+        }
+        .statusBar(hidden: true)
+
+
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
-
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            
+        }
     }
 }
